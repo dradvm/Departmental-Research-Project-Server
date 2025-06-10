ffc41427166f26fe260e2f5a5aa7db93ce0fe9ba
dradvm <duynnvn2511@gmail.com>
2025-06-10 11:28:06 +0700

    change EOF

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { ValidationPipe } from './pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe()
-  );
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
